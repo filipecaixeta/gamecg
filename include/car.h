4ae39dd2e1484f60134158a69e9b6f2d2deb96af
Filipe <filipecaixeta@gmail.com>
2015-05-14 16:35:57 +0900

    added car class

---
+++ include/car.h
@@ -0,0 +1,27 @@
+#ifndef	CAR_H
+#define CAR_H
+#include <string>
+#include <vector>
+#include <iostream>
+#include <shader.h>
+#include <model.h>
+#include <glm/glm.hpp>
+using std::string;
+using std::vector;
+using namespace glm;
+
+class Car
+{
+public:
+	Car(string path);
+	~Car();
+	void updateFrontWheel(float ang);
+	void spinWheel(float ang);
+	void setColor(vec4 color);
+	void updatePosition();
+	void draw(Shader *shader);
+// private:
+	Model model;
+};
+
+#endif
---
+++ src/car.cpp
@@ -0,0 +1,40 @@
+#include <car.h>
+
+Car::Car(string path):model(path)
+{
+	for(auto& mesh: model.meshes)
+	{
+		if (mesh.material->name.find("Glass")==0)
+			mesh.material->color_transparent=vec4(1.0f,1.0f,1.0f,0.6f);
+		else if (mesh.material->name.find("BreakLight")==0)
+			mesh.material->color_emissive=vec4(1.0f,0.0f,0.0f,1.0f);
+	}
+}
+void Car::updateFrontWheel(float ang)
+{
+
+}
+void Car::spinWheel(float ang)
+{
+
+}
+void Car::setColor(vec4 color)
+{
+	for(auto& mesh: model.meshes)
+	{
+		if (mesh.material->name.find("Body")==0)
+			mesh.material->color_diffuse=color;
+	}
+}
+void Car::updatePosition()
+{
+
+}
+void Car::draw(Shader *shader)
+{
+	model.draw(shader);
+}
+Car::~Car()
+{
+	
+}
